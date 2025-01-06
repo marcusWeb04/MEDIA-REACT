56dd52221ba0f92d0444a5d0b94fdccfbb1df619
MarcsWeb04 <marcusfavernay@gmail.com>
2025-01-06 14:10:10 +0100

    modification du router

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NoFound from "./pages/NoFoundPage";
 // public
 import Home from "./pages/Public/Home";
-import Connexion from "./pages/Public/Connexion";
 // client
-import Feed from "./pages/Client/Feed";
-import Music from "./pages/Client/Music";
-import Profil from "./pages/Client/Profil";
-import Follow from "./pages/Client/Follow";
+import Dashborad from "./pages/Client/Dashboard";
 
 // state
 const router = createBrowserRouter([
@@ -18,31 +14,8 @@ const router = createBrowserRouter([
     errorElement: <NoFound />,
   },
   {
-    path: "/connexion",
-    element: <Connexion />,
-  },
-  {
-    path: "/profil",
-    element: <Profil />,
-  },
-  {
-    path: "/feed",
-    element: <Feed />,
-  },
-  {
-    path: "/music",
-    element: <Music />,
-  },
-  {
-    path: "/playlist",
-    element: <Music />,
-  },
-  {
-    path: "/follow",
-    element: <Follow />,
-  },
-  {
-    path: "/setting",
+    path: "/dashborad",
+    element: <Dashborad />,
   },
 ]);
 
--- src/pages/Public/Home.jsx
+++ src/pages/Public/Home.jsx
@@ -7,7 +7,14 @@ import { NavLink } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <section id="home"></section>
+      <section id="home">
+        <form>
+          <div>
+            <label htmlFor="email">email:</label>
+            <input type="text" name="" />
+          </div>
+        </form>
+      </section>
     </>
   );
 }
